2287e0e569bcfdf3919e6224949ee09d1a07e714
Paweł Dąbrowski <dabrowskip9@gmail.com>
2022-12-01 04:56:32 +0100

    Remove unused utils file

--- advent_of_code_2022/utils.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-def load_input(day: int, test=False) -> str:
-    folder = "inputs" if test is True else "test_inputs"
-    return Path(f"advent_of_code_2022/{folder}/day{day:02}.txt").read_text()
