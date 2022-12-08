2a3dbaf858a09bf1bae566e00a364d6b60cbe0da
Paweł Dąbrowski <dabrowskip9@gmail.com>
2022-12-08 14:00:51 +0100

    day 8

--- advent_of_code_2022/day08/day08.py
+++ advent_of_code_2022/day08/day08.py
@@ -1,4 +1,4 @@
-def analyze_trees(input: str):
+def part_1(input: str) -> int:
     lines = input.splitlines()
     rows_n = len(lines)
     cols_n = len(lines[0])
@@ -41,13 +41,53 @@ def analyze_trees(input: str):
                 visible_trees[row_idx][col_idx] = True
                 max_bottom[col_idx] = tree
 
-    return visible_trees
-
-
-def part_1(input: str) -> int:
-    visible_trees = analyze_trees(input)
     return sum([sum(row) for row in visible_trees])
 
 
 def part_2(input: str) -> int:
-    pass
+    lines = input.splitlines()
+    rows_n = len(lines)
+    cols_n = len(lines[0])
+
+    max_score = 0
+
+    for row_idx, row in list(enumerate(lines))[1:-1]:
+        for col_idx, tree in list(enumerate(row))[1:-1]:
+            up, down, left, right = 0, 0, 0, 0
+            i = 1
+
+            while up == 0 or left == 0 or down == 0 or right == 0:
+                if up == 0:
+                    if row_idx - i >= 0:
+                        if lines[row_idx - i][col_idx] >= tree:
+                            up = i
+                    else:
+                        up = row_idx
+
+                if down == 0:
+                    if row_idx + i < rows_n:
+                        if lines[row_idx + i][col_idx] >= tree:
+                            down = i
+                    else:
+                        down = rows_n - row_idx - 1
+
+                if left == 0:
+                    if col_idx - i >= 0:
+                        if lines[row_idx][col_idx - i] >= tree:
+                            left = i
+                    else:
+                        left = col_idx
+
+                if right == 0:
+                    if col_idx + i < cols_n:
+                        if lines[row_idx][col_idx + i] >= tree:
+                            right = i
+                    else:
+                        right = cols_n - col_idx - 1
+
+                i += 1
+
+            score = up * down * left * right
+            max_score = max(score, max_score)
+
+    return max_score
