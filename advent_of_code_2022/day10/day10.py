d28f54b4f2520448974201310df3854ab68c44f4
Paweł Dąbrowski <dabrowskip9@gmail.com>
2022-12-10 16:17:27 +0100

    day 10

---
+++ advent_of_code_2022/day10/day10.py
@@ -0,0 +1,51 @@
+class CPU:
+    def __init__(self, program: str):
+        self.register = 1
+        self.cycle = 1
+        self.history = []
+        self.run_program(program)
+
+    def run_program(self, program: str):
+        for instruction in program.splitlines():
+            match instruction.split(" "):
+                case ["noop"]:
+                    self.tick()
+                case ["addx", value]:
+                    self.addx(int(value))
+        return self
+
+    def addx(self, value: int):
+        self.tick()
+        self.tick()
+        self.register += value
+
+    def tick(self):
+        self.cycle += 1
+        self.history.append(self.register)
+
+    def display_screen(self) -> str:
+        screen = ""
+        for cycle, register in enumerate(self.history):
+            if cycle % 40 == 0:
+                screen += "\n"
+            if cycle % 40 in range(register - 1, register + 2):
+                screen += "#"
+            else:
+                screen += "."
+        return screen
+
+    def signal_strength(self) -> int:
+        return sum(
+            [
+                cycle * self.history[cycle - 1]
+                for cycle in range(20, len(self.history), 40)
+            ]
+        )
+
+
+def part_1(input: str) -> int:
+    return CPU(input).signal_strength()
+
+
+def part_2(input: str) -> str:
+    return CPU(input).display_screen()
